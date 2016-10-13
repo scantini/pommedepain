bd5cad06b8bb638d2d460b59195724b485a15576
Sebastien Cantini <scantini@scantini-ltm.internal.salesforce.com>
2016-10-13 15:51:15 +0200

    status francais

--- client/js/status.js
+++ client/js/status.js
@@ -14,13 +14,13 @@ angular.module('nibs.status', [])
             if (data.originalStatus !== data.newStatus) {
                 $rootScope.user.status = data.newStatus;
                 $ionicPopup.show({
-                    title: 'Congratulations, ' + $rootScope.user.firstname + '!',
+                    title: 'Félications, ' + $rootScope.user.firstname + '!',
                     template:
                         '<div style="text-align: center" class="status' + data.newStatus + '">' +
-                        '<p>You have achieved a new status!</p>' +
-                        '<img src="css/img/icon-status' + data.newStatus + '.svg" height="120"/>' +
-                        '<h3 class="status' + data.newStatus + '">' + STATUS_LABELS[data.newStatus - 1] + ' Member</h3>' +
-                        '<h4 class="status' + data.newStatus + '">' + data.newBalance + ' Nibs</h4>',
+                        '<p>Vous avez atteint un nouveau statut !</p>' +
+                        '<img src="css/img/icon-status' + data.newStatus + '.png" height="120"/>' +
+                        '<h3 class="status' + data.newStatus + '">Membre ' + STATUS_LABELS[data.newStatus - 1] + ' </h3>' +
+                        '<h4 class="status' + data.newStatus + '">' + data.newBalance + ' points</h4>',
                     buttons: [{
                         text: 'OK'
                     }]
