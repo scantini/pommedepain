7cba92a4264199698944ec5c7b4767cc57d8e755
Sebastien Cantini <scantini@scantini-ltm.internal.salesforce.com>
2016-10-13 15:44:13 +0200

    no shadow for map

--- client/js/store-locator.js
+++ client/js/store-locator.js
@@ -34,11 +34,11 @@ angular.module('nibs.store-locator', [])
 
             icon = L.icon({
                 iconUrl: 'img/pommedepainfb.png',
-                shadowUrl: 'img/leaf-shadow.png',
+                //shadowUrl: 'img/leaf-shadow.png',
                 iconSize:     [38, 38], // size of the icon
-                shadowSize:   [50, 64], // size of the shadow
-                iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-                shadowAnchor: [4, 62],  // the same for the shadow
+                //shadowSize:   [50, 64], // size of the shadow
+                iconAnchor:   [22, 22], // point of the icon which will correspond to marker's location
+                //shadowAnchor: [4, 62],  // the same for the shadow
                 popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
             });
 
